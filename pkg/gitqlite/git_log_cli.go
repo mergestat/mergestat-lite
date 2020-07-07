35b33efba8e4612253fa44deb113ed448135844e
vialeon <dnewberry21@amherst.edu>
2020-07-07 18:32:43 -0400

    separate CLI parsing from sqlite module

--- pkg/gitqlite/git_log_cli.go
+++ pkg/gitqlite/git_log_cli.go
@@ -1,13 +1,7 @@
 package gitqlite
 
 import (
-	"bufio"
-	"context"
 	"fmt"
-	"io"
-	"io/ioutil"
-	"os/exec"
-	"strconv"
 	"strings"
 	"time"
 
@@ -21,21 +15,6 @@ type gitLogCLITable struct {
 	repoPath string
 	repo     *git.Repository
 }
-type Commit struct {
-	SHA            string
-	Parent         string
-	Tree           string
-	Message        string
-	AuthorName     string
-	AuthorEmail    string
-	AuthorWhen     time.Time
-	CommitterName  string
-	CommitterEmail string
-	CommitterWhen  time.Time
-	Additions      int
-	Deletions      int
-}
-type Result []*Commit
 
 func (m *gitLogCLIModule) Create(c *sqlite3.SQLiteConn, args []string) (sqlite3.VTab, error) {
 	err := c.DeclareVTab(fmt.Sprintf(`
@@ -71,131 +50,9 @@ func (m *gitLogCLIModule) Connect(c *sqlite3.SQLiteConn, args []string) (sqlite3
 
 func (m *gitLogCLIModule) DestroyModule() {}
 
-func parseLog(reader io.Reader) (Result, error) {
-	scanner := bufio.NewScanner(reader)
-	res := make(Result, 0)
-
-	// line prefixes for the `fuller` formatted output
-	const (
-		commit     = "commit "
-		tree       = "tree "
-		parent     = "parent "
-		author     = "Author: "
-		authorDate = "AuthorDate: "
-		message    = "Message: "
-		committer  = "Commit: "
-		commitDate = "CommitDate: "
-	)
-
-	var currentCommit *Commit
-	for scanner.Scan() {
-		line := scanner.Text()
-		switch {
-		case strings.HasPrefix(line, commit):
-			if currentCommit != nil { // if we're seeing a new commit but already have a current commit, we've finished a commit
-				res = append(res, currentCommit)
-			}
-			currentCommit = &Commit{
-				SHA: strings.TrimPrefix(line, commit),
-			}
-		case strings.HasPrefix(line, tree):
-			currentCommit.Tree = strings.TrimPrefix(line, tree)
-		case strings.HasPrefix(line, parent):
-			currentCommit.Parent = strings.TrimPrefix(line, parent)
-		case strings.HasPrefix(line, author):
-			s := strings.TrimPrefix(line, author)
-			spl := strings.Split(s, " ")
-			email := strings.Trim(spl[len(spl)-1], "<>")
-			name := strings.Join(spl[:len(spl)-1], " ")
-			currentCommit.AuthorEmail = strings.Trim(email, "<>")
-			currentCommit.AuthorName = strings.TrimSpace(name)
-		case strings.HasPrefix(line, authorDate):
-			authorDateString := strings.TrimPrefix(line, authorDate)
-			aD, err := time.Parse(time.RFC3339, authorDateString)
-			if err != nil {
-				return nil, err
-			}
-			currentCommit.AuthorWhen = aD
-		case strings.HasPrefix(line, committer):
-			s := strings.TrimPrefix(line, committer)
-			spl := strings.Split(s, " ")
-			email := strings.Trim(spl[len(spl)-1], "<>")
-			name := strings.Join(spl[:len(spl)-1], " ")
-			currentCommit.CommitterEmail = strings.Trim(email, "<>")
-			currentCommit.CommitterName = strings.TrimSpace(name)
-		case strings.HasPrefix(line, commitDate):
-			commitDateString := strings.TrimPrefix(line, commitDate)
-			cD, err := time.Parse(time.RFC3339, commitDateString)
-			if err != nil {
-				return nil, err
-			}
-			currentCommit.CommitterWhen = cD
-		case strings.HasPrefix(line, message):
-			currentCommit.Message = strings.TrimPrefix(line, message)
-		case strings.TrimSpace(line) == "": // ignore empty lines
-		default:
-			s := strings.Split(line, "\t")
-			var additions int
-			var deletions int
-			var err error
-			if s[0] != "-" {
-				additions, err = strconv.Atoi(s[0])
-				if err != nil {
-					return nil, err
-				}
-			}
-			if s[1] != "-" {
-				deletions, err = strconv.Atoi(s[1])
-				if err != nil {
-					return nil, err
-				}
-			}
-			currentCommit.Additions = additions
-			currentCommit.Deletions = deletions
-		}
-	}
-	if currentCommit != nil {
-		res = append(res, currentCommit)
-	}
-	fmt.Println(res)
-	return res, nil
-}
-
 func (v *gitLogCLITable) Open() (sqlite3.VTabCursor, error) {
-	gitPath, err := exec.LookPath("git")
+	res, err := ExecuteParse(v.repoPath)
 	if err != nil {
-		return nil, err
-	}
-	args := []string{"log"}
-	args = append(args, "--format=commit %H %ntree %T%nparent %P%nAuthor: %an %ae%nAuthorDate: %aI%nCommit: %cn %ce%nCommitDate: %cI%nMessage: %s", "--numstat")
-	cmd := exec.CommandContext(context.Background(), gitPath, args...)
-	cmd.Dir = v.repoPath
-
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		return nil, err
-	}
-
-	stderr, err := cmd.StderrPipe()
-	if err != nil {
-		return nil, err
-	}
-
-	if err := cmd.Start(); err != nil {
-		return nil, err
-	}
-
-	res, err := parseLog(stdout)
-	if err != nil {
-		return nil, err
-	}
-
-	errs, err := ioutil.ReadAll(stderr)
-	if err != nil {
-		return nil, err
-	}
-	if err := cmd.Wait(); err != nil {
-		fmt.Println(string(errs))
 		return nil, err
 	}
 	return &commitCLICursor{0, res, res[0], false}, nil
