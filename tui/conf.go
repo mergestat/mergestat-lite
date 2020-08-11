e66385a00371d27dd57f80ccb5d43ed829e3f74c
vialeon <dnewberry21@amherst.edu>
2020-08-11 11:03:49 -0400

    formatting

--- tui/conf.go
+++ tui/conf.go
@@ -8,22 +8,25 @@ var (
 		[]string{`SELECT 
 		author_email, count(*) 
 		FROM commits GROUP BY author_email 
-		ORDER BY count(*) DESC`, `number of commits for each author by email`},
+		ORDER BY count(*) DESC`,
+			`number of commits for each author by email`},
 		[]string{`SELECT 
-count(*) AS commits, SUM(additions) AS additions, SUM(deletions) AS  deletions, author_email 
-FROM commits 
-GROUP BY author_email
-ORDER BY commits`, `number of additions and deletions for each author`},
+		count(*) AS commits, SUM(additions) AS additions, SUM(deletions) AS  deletions, author_email 
+		FROM commits 
+		GROUP BY author_email
+		ORDER BY commits`,
+			`number of additions and deletions for each author`},
 		[]string{`SELECT
-count(*) AS commits,
-count(CASE WHEN strftime('%w',author_when)='0' THEN 1 END) AS sunday,
-count(CASE WHEN strftime('%w',author_when)='1' THEN 1 END) AS monday,
-count(CASE WHEN strftime('%w',author_when)='2' THEN 1 END) AS tuesday,
-count(CASE WHEN strftime('%w',author_when)='3' THEN 1 END) AS wednesday,
-count(CASE WHEN strftime('%w',author_when)='4' THEN 1 END) AS thursday,
-count(CASE WHEN strftime('%w',author_when)='5' THEN 1 END) AS friday,
-count(CASE WHEN strftime('%w',author_when)='6' THEN 1 END) AS saturday,
-author_email
-FROM commits GROUP BY author_email ORDER BY commits`, `number of commits for each author day of the week`},
+		count(*) AS commits,
+		count(CASE WHEN strftime('%w',author_when)='0' THEN 1 END) AS sunday,
+		count(CASE WHEN strftime('%w',author_when)='1' THEN 1 END) AS monday,
+		count(CASE WHEN strftime('%w',author_when)='2' THEN 1 END) AS tuesday,
+		count(CASE WHEN strftime('%w',author_when)='3' THEN 1 END) AS wednesday,
+		count(CASE WHEN strftime('%w',author_when)='4' THEN 1 END) AS thursday,
+		count(CASE WHEN strftime('%w',author_when)='5' THEN 1 END) AS friday,
+		count(CASE WHEN strftime('%w',author_when)='6' THEN 1 END) AS saturday,
+		author_email
+		FROM commits GROUP BY author_email ORDER BY commits`,
+			`number of commits for each author day of the week`},
 	}
 )
