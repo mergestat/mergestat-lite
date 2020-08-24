3089b542b866e38b1e82f76d4f701893e4fc5886
vialeon <dnewberry21@amherst.edu>
2020-08-24 15:16:56 -0400

    baseline stats testing go-git

--- pkg/gitqlite/git_stats_test.go
+++ pkg/gitqlite/git_stats_test.go
@@ -76,57 +76,63 @@ func TestGoGitStats(t *testing.T) {
 	if numRows != expected {
 		t.Fatalf("expected %d rows got: %d", expected, numRows)
 	}
-	/*
-		rows, err = instance.DB.Query("SELECT commit_id, file FROM stats")
-		if err != nil {
-			t.Fatal(err)
-		}
-		rowNum, contents, err := GetContents(rows)
-		if err != nil {
-			t.Fatalf("err %d at row Number %d", err, rowNum)
-		}
-		statsIndex := 0
-		// reset commitchecker
-		commitChecker, err = fixtureRepo.Log(&git.LogOptions{
-			From:  headRef.Hash(),
-			Order: git.LogOrderCommitterTime,
-		})
-		if err != nil {
-			t.Fatal(err)
-		}
 
-		commit, err := commitChecker.Next()
-		if err != nil {
-			t.Fatal(err)
-		}
-		for i, c := range contents {
-			stats, err := commit.Stats()
-			if err != nil {
-				t.Fatal(err)
-			}
+	rows, err = instance.DB.Query("SELECT commit_id, file FROM stats")
+	if err != nil {
+		t.Fatal(err)
+	}
+	/*rowNum, contents, err := GetContents(rows)
+	if err != nil {
+		t.Fatalf("err %d at row Number %d", err, rowNum)
+	}
+	statsIndex := 0
+	// reset commitchecker
+	commitChecker, err = fixtureRepo.Log(&git.LogOptions{
+		From:  headRef.Hash(),
+		Order: git.LogOrderCommitterTime,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
 
-			if commit.ID().String() != c[0] {
-				t.Fatalf("expected %s at row %d got %s", commit.ID().String(), i, c[0])
-			}
-			if stats[statsIndex].Name != c[1] && c[1] != "NULL" {
-				t.Fatalf("expected %s at row %d got %s", stats[statsIndex].Name, i, c[1])
-			}
-			if statsIndex < len(stats)-1 {
-				statsIndex++
-			} else {
-				commit, err = commitChecker.Next()
-				if err != nil {
-					if err == io.EOF {
-						break
-					} else {
-						t.Fatal(err)
-					}
-				}
-				statsIndex = 0
-			}
+	commit, err := commitChecker.Next()
+	if err != nil {
+		t.Fatal(err)
+	}
+	 for the range of contents check against each stat to see if there are discrepancies
+	 for i, c := range contents {
+	 	stats, err := commit.Stats()
+	 	if err != nil {
+	 		t.Fatal(err)
+	 	}
 
-		}
+	 	if commit.ID().String() != c[0] {
+	 		t.Fatalf("expected %s %d %d at row %d got %s", commit.ID().String(), stats[statsIndex].Addition, stats[statsIndex].Deletion, i, c[0])
+	 	}
+	 	if stats[statsIndex].Name != c[1] && c[1] != "NULL" {
+	 		t.Fatalf("expected %s, %d, $d, at row %d got %d %s", stats[statsIndex].Name, stats[statsIndex].Addition, stats[statsIndex].Deletion, i, c[1])
+	 	}
+	 	if statsIndex < len(stats)-1 {
+	 		statsIndex++
+	 	}
+	 	for stats[statsIndex].Addition == 0 && stats[statsIndex].Deletion == 0 && statsIndex < len(stats)-1 {
+	 		statsIndex++
+	 	}
+	 	if statsIndex > len(stats)-1 {
+	 		commit, err = commitChecker.Next()
+	 		if err != nil {
+	 			if err == io.EOF {
+	 				break
+	 			} else {
+	 				t.Fatal(err)
+	 			}
+	 		}
+	 		statsIndex = 0
+	 	}
+
+	 }
 	*/
+
 }
 func BenchmarkGoGitstatsCounts(b *testing.B) {
 	for i := 0; i < b.N; i++ {
