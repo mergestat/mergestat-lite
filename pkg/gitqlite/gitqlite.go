d63250ba41b1ae5f3bfc28d566073e7732f93560
Patrick DeVivo <patrick.devivo@gmail.com>
2020-10-08 11:40:43 -0400

    loads a helper function for splitting strings

--- pkg/gitqlite/gitqlite.go
+++ pkg/gitqlite/gitqlite.go
@@ -48,6 +48,11 @@ func init() {
 				return err
 			}
 
+			err = loadHelperFuncs(conn)
+			if err != nil {
+				return err
+			}
+
 			return nil
 		},
 	})
@@ -104,6 +109,23 @@ func (g *GitQLite) ensureTables(options *Options) error {
 	}
 	_, err = g.DB.Exec(fmt.Sprintf("CREATE VIRTUAL TABLE IF NOT EXISTS branches USING git_branch('%s');", g.RepoPath))
 	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func loadHelperFuncs(conn *sqlite3.SQLiteConn) error {
+	// str_split(inputString, splitCharacter, index) string
+	split := func(s, c string, i int) string {
+		split := strings.Split(s, c)
+		if i < len(split) {
+			return split[i]
+		}
+		return ""
+	}
+
+	if err := conn.RegisterFunc("str_split", split, true); err != nil {
 		return err
 	}
 
