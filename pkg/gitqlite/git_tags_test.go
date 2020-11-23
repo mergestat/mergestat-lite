5b902936bc3f0455bcffb6ec01b9f2b33b70d1e7
vialeon <dnewberry21@amherst.edu>
2020-11-23 13:42:01 -0500

    tags table testing

--- pkg/gitqlite/git_tags_test.go
+++ pkg/gitqlite/git_tags_test.go
@@ -1,51 +1,35 @@
 package gitqlite
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestTags(t *testing.T) {
-	instance, err := New(fixtureRepoDir, &Options{})
-	if err != nil {
-		t.Fatal(err)
+	testCases := []test{
+		{"checkTags", "SELECT COUNT(*) FROM tags", numTags(t)},
 	}
+	for _, tc := range testCases {
+		expected := tc.want
+		results := runQuery(t, tc.query)
+		if len(expected) != len(results) {
+			t.Fatalf("expected %d entries got %d, test: %s, %s, %s", len(expected), len(results), tc.name, expected, results)
+		}
+		for x := 0; x < len(expected); x++ {
+			if results[x] != expected[x] {
+				t.Fatalf("expected %s, got %s, test %s", expected[x], results[x], tc.name)
+			}
+		}
+	}
+}
 
+func numTags(t *testing.T) []string {
 	tags, err := fixtureRepo.Tags.List()
 	if err != nil {
 		t.Fatal(err)
 	}
+	return []string{fmt.Sprintf("%d", len(tags))}
 
-	tagRows, err := instance.DB.Query("SELECT * FROM tags")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	rowNum, contents, err := GetContents(tagRows)
-	if err != nil {
-		t.Fatalf("err %d at row Number %d", err, rowNum)
-	}
-
-	if len(contents) != len(tags) {
-		t.Fatalf("mismatched count of tags, expected %d got %d", len(tags), len(contents))
-	}
-
-	// for i, c := range contents {
-	// 	tag, err := tagIterator.Next()
-	// 	if err != nil {
-	// 		if err == io.EOF {
-	// 			break
-	// 		} else {
-	// 			t.Fatal(err)
-	// 		}
-	// 	}
-	// 	if tag.Hash().String() != c[0] {
-	// 		t.Fatalf("expected %s at row %d got %s", tag.Hash().String(), i, c[0])
-	// 	}
-	// 	if tag.Name().String() != c[1] {
-	// 		t.Fatalf("expected %s at row %d got %s", tag.Name(), i, c[1])
-	// 	}
-
-	// }
 }
 func BenchmarkTagsCounts(b *testing.B) {
 	for i := 0; i < b.N; i++ {
