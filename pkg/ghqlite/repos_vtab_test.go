42c639633e1e8de56fc961e6722c80fabac981c5
Patrick DeVivo <patrick.devivo@gmail.com>
2020-11-27 22:13:58 -0500

    Update repos_vtab_test.go

--- pkg/ghqlite/repos_vtab_test.go
+++ pkg/ghqlite/repos_vtab_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestReposTable(t *testing.T) {
-	_, err := DB.Query("SELECT count(*) FROM repos")
+	_, err := DB.Query("SELECT * FROM repos LIMIT 5")
 	if err != nil {
 		t.Fatal(err)
 	}
