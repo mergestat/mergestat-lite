494debdd412d19fe3321ed5477df135b8ae29f12
vialeon <dnewberry21@amherst.edu>
2020-12-01 19:34:48 -0500

    commits table testing

--- pkg/gitqlite/git_log_test.go
+++ pkg/gitqlite/git_log_test.go
@@ -2,83 +2,148 @@ package gitqlite
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
 	git "github.com/libgit2/git2go/v30"
 )
 
+type test struct {
+	name  string
+	query string
+	want  []string
+}
+
 func TestCommits(t *testing.T) {
+
+	//this only works if you test one thing at a time.
+	testCases := []test{
+		{"checkCommits", "SELECT COUNT(*) FROM commits", getCommitCount(t)},
+		{"getAuthors", "SELECT author_name FROM commits", getAuthors(t)},
+		{"getID's", "SELECT id FROM commits", getIDs(t)},
+	}
+	for _, tc := range testCases {
+		expected := tc.want
+		results := runQuery(t, tc.query)
+		if len(expected) != len(results) {
+			t.Fatalf("expected %d entries got %d, test: %s, %s, %s", len(expected), len(results), tc.name, expected, results)
+		}
+		for x := 0; x < len(expected); x++ {
+			if results[x] != expected[x] {
+				t.Fatalf("expected %s, got %s, test %s", expected[x], results[x], tc.name)
+			}
+		}
+	}
+
+}
+func runQuery(t *testing.T, query string) []string {
+	//checks commits
+	rows, err := fixtureDB.Query(query)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer rows.Close()
+	rowNum, contents, err := GetContents(rows)
+	if err != nil {
+		t.Fatalf("err %d at row Number %d", err, rowNum)
+	}
+	var ret []string
+	for _, entry := range contents {
+		ret = append(ret, entry...)
+	}
+
+	return ret
+}
+func getAuthors(t *testing.T) []string {
+	//revWalk := createRevWalk(t)
 	revWalk, err := fixtureRepo.Walk()
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer revWalk.Free()
 
 	err = revWalk.PushHead()
 	if err != nil {
 		t.Fatal(err)
 	}
+	var authors []string
 
-	commitCount := 0
-	err = revWalk.Iterate(func(c *git.Commit) bool {
-		commitCount++
-		return true
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	//checks commits
-	rows, err := fixtureDB.Query("SELECT * FROM commits")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer rows.Close()
-
-	columns, err := rows.Columns()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	expected := 12
-	if len(columns) != expected {
-		t.Fatalf("expected %d columns, got: %d", expected, len(columns))
-	}
-	numRows := GetRowsCount(rows)
-
-	expected = commitCount
-	if numRows != expected {
-		t.Fatalf("expected %d rows got: %d", expected, numRows)
-	}
-
-	rows, err = fixtureDB.Query("SELECT id, author_name FROM commits")
-	if err != nil {
-		t.Fatal(err)
-	}
-	rowNum, contents, err := GetContents(rows)
-	if err != nil {
-		t.Fatalf("err %d at row Number %d", err, rowNum)
-	}
-
-	i := 0
 	err = revWalk.Iterate(func(commit *git.Commit) bool {
-		c := contents[i]
-		if commit.Id().String() != c[0] {
-			t.Fatalf("expected %s at row %d got %s", commit.Id().String(), i, c[0])
-		}
-
-		if commit.Author().Name != c[1] {
-			t.Fatalf("expected %s at row %d got %s", commit.Author().Name, i, c[1])
-		}
-
-		i++
+		authors = append(authors, commit.Author().Name)
 		return true
 	})
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	return authors
+}
+func getIDs(t *testing.T) []string {
+	revWalk, err := fixtureRepo.Walk()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = revWalk.PushHead()
+	if err != nil {
+		t.Fatal(err)
+	}
+	var IDs []string
+	err = revWalk.Iterate(func(commit *git.Commit) bool {
+		IDs = append(IDs, commit.Id().String())
+		return true
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return IDs
+}
+func getCommitCount(t *testing.T) []string {
+	//revWalk := createRevWalk(t)
+	revWalk, err := fixtureRepo.Walk()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = revWalk.PushHead()
+	if err != nil {
+		t.Fatal(err)
+	}
+	count := 0
+	err = revWalk.Iterate(func(commit *git.Commit) bool {
+		count++
+		return true
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	r := fmt.Sprintf("%d", count)
+	ret := strings.Split(r, " ")
+
+	//revWalk.Free()
+	return ret
 }
 
+// func createRevWalk(t *testing.T) *git.RevWalk {
+// 	revWalk, err := fixtureRepo.Walk()
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	defer revWalk.Free()
+
+// func createRevWalk(t *testing.T) *git.RevWalk {
+// 	revWalk, err := fixtureRepo.Walk()
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	defer revWalk.Free()
+
+// 	err = revWalk.PushHead()
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	return revWalk
+// }
 func TestCommitByID(t *testing.T) {
 	o, err := fixtureRepo.RevparseSingle("HEAD~3")
 	if err != nil {
