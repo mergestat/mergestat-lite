19c0b9aa54519d8cdbec82734b06cee9608bd17d
Patrick DeVivo <patrick.devivo@gmail.com>
2020-12-26 15:28:24 -0500

    use a different term width pkg, add a test case

--- pkg/askgit/display.go
+++ pkg/askgit/display.go
@@ -7,7 +7,7 @@ import (
 	"io"
 
 	"github.com/jedib0t/go-pretty/table"
-	"golang.org/x/crypto/ssh/terminal"
+	"golang.org/x/term"
 )
 
 func DisplayDB(rows *sql.Rows, w io.Writer, format string) error {
@@ -161,9 +161,11 @@ func tableDisplay(rows *sql.Rows, write io.Writer) error {
 		pointers[i] = &container[i]
 	}
 
-	width, _, err := terminal.GetSize(0)
+	width, _, err := term.GetSize(0)
 	if err != nil {
-		return err
+		// TODO - getting terminal size seems to fail with `operation not supported by device` in tests
+		// as a workaround for now, set a default width instead of returning an error, if one is encountered
+		width = 500
 	}
 
 	t := table.NewWriter()
