22c3ce12c4289b82d978b9bdb6669bf3b08a3d68
vialeon <dnewberry21@amherst.edu>
2021-02-15 10:56:26 -0500

    add testing for *_to_json functions

--- pkg/askgit/helpers_test.go
+++ pkg/askgit/helpers_test.go
@@ -36,3 +36,65 @@ func TestStrSplit(t *testing.T) {
 		t.Fatalf("expected string: %s, got %s", "", contents[0][0])
 	}
 }
+func TestTomlToJson(t *testing.T) {
+	ag, err := New(&Options{RepoPath: fixtureRepoDir})
+	if err != nil {
+		t.Fatal(err)
+	}
+	rows, err := ag.DB().Query(`SELECT toml_to_json('[package] 
+	name = \"hog\"')`)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rowNum, contents, err := GetContents(rows)
+	if err != nil {
+		t.Fatalf("err %d at row Number %d", err, rowNum)
+	}
+	if contents[0][0] != "{\"package\":{\"name\":\"hog\"}}" {
+		t.Fatalf("expected string: %s, got %s", "", contents[0][0])
+	}
+}
+func TestYmlToJson(t *testing.T) {
+	ag, err := New(&Options{RepoPath: fixtureRepoDir})
+	if err != nil {
+		t.Fatal(err)
+	}
+	rows, err := ag.DB().Query(`SELECT yml_to_json('
+	---
+	doe: "a deer, a female deer"'
+	)`)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rowNum, contents, err := GetContents(rows)
+	if err != nil {
+		t.Fatalf("err %d at row Number %d", err, rowNum)
+	}
+	if contents[0][0] != `"doe":"a deer, a female deer"` {
+		t.Fatalf("expected string: %s, got %s", "", contents[0][0])
+	}
+
+}
+func TestXmlToJson(t *testing.T) {
+	ag, err := New(&Options{RepoPath: fixtureRepoDir})
+	if err != nil {
+		t.Fatal(err)
+	}
+	rows, err := ag.DB().Query(`SELECT xml_to_json('
+	<?xml version ="1.0" encoding="UTF-8"?>
+	<employee>
+		<fname>john</fname>
+		<lname>doe</lname>
+		<home>neverland</home>
+	</employee>')`)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rowNum, contents, err := GetContents(rows)
+	if err != nil {
+		t.Fatalf("err %d at row Number %d", err, rowNum)
+	}
+	if contents[0][0] != `{"employee":{"fname":"john","home":"neverland","lname":"doe"}}` {
+		t.Fatalf("expected string: %s, got %s", "", contents[0][0])
+	}
+}
