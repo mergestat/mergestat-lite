9c76552a297aa230ac71a9f3b9480826be51af10
vialeon <dnewberry21@amherst.edu>
2021-03-01 13:43:49 -0500

    rm commented out code

--- pkg/gitqlite/blame_test.go
+++ pkg/gitqlite/blame_test.go
@@ -54,31 +54,6 @@ func TestBlameDistinctFiles(t *testing.T) {
 	}
 
 }
-
-// func TestBlameContents(t *testing.T) {
-// 	iterator, err := NewBlameIterator(fixtureRepo)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// 	rows, err := fixtureDB.Query("SELECT line_content from blame limit 100")
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// 	_, lines, err := GetRowContents(rows)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// 	for _, line := range lines {
-// 		cont, err := iterator.Next()
-// 		if err != nil {
-// 			t.Fatal(err)
-// 		}
-// 		if !(line[0] == cont.Content) {
-// 			t.Fatalf("expected %s content in blame, got %s", cont.Content, line[0])
-// 		}
-// 	}
-// }
-// I need to pass the file name fo the blame
 func TestBlameCommitID(t *testing.T) {
 	iterator, err := NewBlameIterator(fixtureRepo)
 	if err != nil {
@@ -106,26 +81,3 @@ func TestBlameCommitID(t *testing.T) {
 		}
 	}
 }
-
-// func TestBlameFileNames(t *testing.T) {
-// 	iterator, err := NewBlameIterator(fixtureRepo)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// 	rows, err := fixtureDB.Query("SELECT file_path from blame limit 100")
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// 	_, lines, err := GetRowContents(rows)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// 	results, err := blame.Exec(context.Background(),fixtureRepoDir,&blame.Options{})
-
-// 	for i, line := range lines {
-// 		if !(line[0] == results[i].) {
-// 			t.Fatalf("expected %s content in blame, got %s", results[i].SHA, line[0])
-// 		}
-// 	}
-// 	}
-//}
