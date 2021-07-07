616c49d4cf196aa1fb0208df39c39cb7d895bf40
Riyaz Ali <me@riyazali.net>
2021-07-07 22:15:24 +0530

    feat: update share extension entry-point

--- shared.go
+++ shared.go
@@ -4,6 +4,17 @@
 // It imports github.com/augmentable-dev/askgit/tables which provides the actual extension implementation.
 package main
 
-import _ "github.com/augmentable-dev/askgit/tables"
+import (
+	"github.com/augmentable-dev/askgit/pkg/locator"
+	"github.com/augmentable-dev/askgit/tables"
+	"go.riyazali.net/sqlite"
+)
+
+func init() {
+	sqlite.Register(tables.RegisterFn(
+		tables.WithExtraFunctions(),
+		tables.WithRepoLocator(locator.CachedLocator(locator.MultiLocator())),
+	))
+}
 
 func main() { /* noting here fellas */ }
