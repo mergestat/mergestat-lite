c67e2b455a6962bc5d7f01df328e1cdf93e9c3fd
Patrick DeVivo <patrick.devivo@gmail.com>
2021-07-19 16:47:42 -0400

    add `TestSelectKnownContents` for the `files` table

--- tables/internal/git/files_test.go
+++ tables/internal/git/files_test.go
@@ -1,6 +1,8 @@
 package git_test
 
 import (
+	"fmt"
+	"io/ioutil"
 	"testing"
 )
 
@@ -22,6 +24,39 @@ func TestSelect10FilesHEAD(t *testing.T) {
 			t.Fatalf("failed to scan resultset: %v", err)
 		}
 		t.Logf("file: path=%s executable=%d contents_len=%d", path, executable, len(contents))
+	}
+
+	if err = rows.Err(); err != nil {
+		t.Fatalf("failed to fetch results: %v", err.Error())
+	}
+}
+
+func TestSelectKnownContents(t *testing.T) {
+	db := Connect(t, Memory)
+	repo, hash := "https://github.com/askgitdev/askgit", "2359c9a9ba0ba8aa694601ff12538c4e74b82cd5"
+
+	rows, err := db.Query("SELECT path, contents FROM files(?, ?) WHERE path LIKE 'Makefile' OR path LIKE 'go.mod'", repo, hash)
+	if err != nil {
+		t.Fatalf("failed to execute query: %v", err.Error())
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var path, contents string
+		err = rows.Scan(&path, &contents)
+		if err != nil {
+			t.Fatalf("failed to scan resultset: %v", err)
+		}
+		t.Logf("file: path=%s contents_len=%d", path, len(contents))
+
+		expectedFileContents, err := ioutil.ReadFile(fmt.Sprintf("./testdata/%s/%s.testdata", hash, path))
+		if err != nil {
+			t.Fatalf("failed to load fixture file: %v", err)
+		}
+
+		if contents != string(expectedFileContents) {
+			t.Fatalf("contents of %s files do not match", path)
+		}
 	}
 
 	if err = rows.Err(); err != nil {
