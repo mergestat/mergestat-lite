48772a05d9dd0f52fc35f5463a24cced4372214f
Patrick DeVivo <patrick.devivo@gmail.com>
2021-08-03 17:03:51 -0400

    add some basic test cases

---
+++ tables/internal/github/user_repos_test.go
@@ -0,0 +1,33 @@
+package github_test
+
+import (
+	"testing"
+
+	"github.com/askgitdev/askgit/tables/internal/tools"
+)
+
+func TestUserRepos(t *testing.T) {
+	cleanup := newRecorder(t)
+	defer cleanup()
+
+	db := Connect(t, Memory)
+
+	rows, err := db.Query("SELECT * FROM github_user_repos('josephjacks') LIMIT 10")
+	if err != nil {
+		t.Fatalf("failed to execute query: %v", err.Error())
+	}
+	defer rows.Close()
+
+	colCount, content, err := tools.RowContent(rows)
+	if err != nil {
+		t.Fatalf("failed to retrieve row contents: %v", err.Error())
+	}
+
+	if colCount != 6 {
+		t.Fatalf("expected 6 columns, got: %d", colCount)
+	}
+
+	if len(content) != 10 {
+		t.Fatalf("expected 10 rows, got: %d", len(content))
+	}
+}
