03544cc4d027f1ed76a45be1def85449413562c6
vialeon <dnewberry21@amherst.edu>
2021-09-21 20:30:24 -0500

    preliminary testing for repo protections

---
+++ extensions/internal/github/repo_branch_protection_test.go
@@ -0,0 +1,33 @@
+package github_test
+
+import (
+	"testing"
+
+	"github.com/askgitdev/askgit/extensions/internal/tools"
+)
+
+func TestRepoProtections(t *testing.T) {
+	cleanup := newRecorder(t)
+	defer cleanup()
+
+	db := Connect(t, Memory)
+
+	rows, err := db.Query("SELECT * FROM github_protections('askgitdev','askgit') LIMIT 10")
+	if err != nil {
+		t.Fatalf("failed to execute query: %v", err.Error())
+	}
+	defer rows.Close()
+
+	colCount, content, err := tools.RowContent(rows)
+	if err != nil {
+		t.Fatalf("failed to retrieve row contents: %v", err.Error())
+	}
+
+	if colCount != 18 {
+		t.Fatalf("expected 29 columns, got: %d", colCount)
+	}
+
+	if len(content) != 10 {
+		t.Fatalf("expected 10 rows, got: %d", len(content))
+	}
+}
