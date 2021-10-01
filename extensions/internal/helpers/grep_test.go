086c8556f6a30cf4250513a8346ec4dd41bd0f4e
Patrick DeVivo <patrick.devivo@gmail.com>
2021-10-01 09:30:36 -0400

    add some tests for before/after line preview in `grep`

--- extensions/internal/helpers/grep_test.go
+++ extensions/internal/helpers/grep_test.go
@@ -1,6 +1,7 @@
 package helpers
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/askgitdev/askgit/extensions/internal/tools"
@@ -14,6 +15,8 @@ abc
 line3
 line4
 	`
+
+	// no preview
 	rows, err := FixtureDatabase.Query("SELECT * FROM grep(?, 'abc')", dummy)
 	if err != nil {
 		t.Fatal(err)
@@ -30,5 +33,55 @@ line4
 
 	if contents[0][1] != "abc" {
 		t.Fatalf("expected string: %s, got %s", "hello", contents[0][1])
+	}
+
+	// with before lines
+	rows, err = FixtureDatabase.Query("SELECT * FROM grep(?, 'abc', 2)", dummy)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rowNum, contents, err = tools.RowContent(rows)
+	if err != nil {
+		t.Fatalf("err %v at row: %d", err, rowNum)
+	}
+
+	if len(contents) != 1 {
+		t.Fatalf("expected 1 row")
+	}
+
+	preview := strings.Split(contents[0][1], "\n")
+
+	if len(preview) != 3 {
+		t.Fatalf("expected 3 lines of output")
+	}
+
+	if preview[0] != "line1" || preview[1] != "line2" {
+		t.Fatalf("unexpected lines in before")
+	}
+
+	// with after lines
+	rows, err = FixtureDatabase.Query("SELECT * FROM grep(?, 'abc', 0, 2)", dummy)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rowNum, contents, err = tools.RowContent(rows)
+	if err != nil {
+		t.Fatalf("err %v at row: %d", err, rowNum)
+	}
+
+	if len(contents) != 1 {
+		t.Fatalf("expected 1 row")
+	}
+
+	preview = strings.Split(contents[0][1], "\n")
+
+	if len(preview) != 3 {
+		t.Fatalf("expected 3 lines of output")
+	}
+
+	if preview[1] != "line3" || preview[2] != "line4" {
+		t.Fatalf("unexpected lines in before")
 	}
 }
