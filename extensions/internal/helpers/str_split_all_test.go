b4060d66037a7e6b1c8a37286dfde6d2d52195c8
vialeon <dnewberry21@amherst.edu>
2021-10-04 19:35:21 -0500

    add simple testing

---
+++ extensions/internal/helpers/str_split_all_test.go
@@ -0,0 +1,27 @@
+package helpers
+
+import (
+	"testing"
+
+	"github.com/askgitdev/askgit/extensions/internal/tools"
+)
+
+func TestStrSplitAll(t *testing.T) {
+	rows, err := FixtureDatabase.Query("SELECT * from str_split_all('hello,my,name,is,what,my,name,is,who', ',')")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rowNum, contents, err := tools.RowContent(rows)
+	if err != nil {
+		t.Fatalf("err %d at row Number %d", err, rowNum)
+	}
+
+	if contents[0][1] != "hello" {
+		t.Fatalf("expected string: %s, got %s", "hello", contents[0][0])
+	}
+
+	if len(contents) != 9 {
+		t.Fatalf("expected 9 rows instead got %d", len(contents[0]))
+	}
+}
