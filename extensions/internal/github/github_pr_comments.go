c967e39087e2dc181683d7f7d8c162f8ec415b5f
vialeon <dnewberry21@amherst.edu>
2021-10-25 21:03:40 -0500

    working code with dead code removed

--- extensions/internal/github/github_pr_comments.go
+++ extensions/internal/github/github_pr_comments.go
@@ -2,8 +2,6 @@ package github
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
 	"io"
 
 	"github.com/augmentable-dev/vtab"
@@ -123,103 +121,9 @@ func (i *iterPRComments) Column(ctx vtab.Context, c int) error {
 	case "pr_number":
 		ctx.ResultInt(i.results.Edges[i.currentPR].Number)
 	}
-	// case "closed":
-	// 	ctx.ResultInt(t1f0(current.Closed))
-	// case "closed_at":
-	// 	t := current.ClosedAt
-	// 	if t.IsZero() {
-	// 		ctx.ResultNull()
-	// 	} else {
-	// 		ctx.ResultText(t.Format(time.RFC3339Nano))
-	// 	}
-	// case "comment_count":
-	// 	ctx.ResultInt(current.Comments.TotalCount)
-	// case "commit_count":
-	// 	ctx.ResultInt(current.Commits.TotalCount)
-	// case "created_at":
-	// 	t := current.CreatedAt
-	// 	if t.IsZero() {
-	// 		ctx.ResultNull()
-	// 	} else {
-	// 		ctx.ResultText(t.Format(time.RFC3339Nano))
-	// 	}
-	// case "created_via_email":
-	// 	ctx.ResultInt(t1f0(current.CreatedViaEmail))
-	// case "database_id":
-	// 	ctx.ResultInt(current.DatabaseID)
-	// case "deletions":
-	// 	ctx.ResultInt(current.Deletions)
-	// case "editor_login":
-	// 	ctx.ResultText(current.Editor.Login)
-	// case "head_ref_name":
-	// 	ctx.ResultText(current.HeadRefName)
-	// case "head_ref_oid":
-	// 	ctx.ResultText(string(current.HeadRefOid))
-	// case "head_repository_name":
-	// 	ctx.ResultText(string(current.HeadRepository.NameWithOwner))
-	// case "is_draft":
-	// 	ctx.ResultInt(t1f0(current.IsDraft))
-	// case "label_count":
-	// 	ctx.ResultInt(current.Labels.TotalCount)
-	// case "last_edited_at":
-	// 	t := current.LastEditedAt
-	// 	if t.IsZero() {
-	// 		ctx.ResultNull()
-	// 	} else {
-	// 		ctx.ResultText(t.Format(time.RFC3339Nano))
-	// 	}
-	// case "locked":
-	// 	ctx.ResultInt(t1f0(current.Locked))
-	// case "maintainer_can_modify":
-	// 	ctx.ResultInt(t1f0(current.MaintainerCanModify))
-	// case "mergeable":
-	// 	ctx.ResultText(string(current.Mergeable))
-	// case "merged":
-	// 	ctx.ResultInt(t1f0(current.Merged))
-	// case "merged_at":
-	// 	t := current.MergedAt
-	// 	if t.IsZero() {
-	// 		ctx.ResultNull()
-	// 	} else {
-	// 		ctx.ResultText(t.Format(time.RFC3339Nano))
-	// 	}
-	// case "merged_by":
-	// 	ctx.ResultText(current.MergedBy.Login)
-	// case "number":			l. := i.logger().With().Interface("prCursor", prCursor).Logger()
-	// case "participant_count":
-	// 	ctx.ResultInt(current.Participants.TotalCount)
-	// case "published_at":
-	// 	t := current.PublishedAt
-	// 	if t.IsZero() {
-	// 		ctx.ResultNull()
-	// 	} else {
-	// 		ctx.ResultText(t.Format(time.RFC3339Nano))
-	// 	}
-	// case "review_decision":
-	// 	ctx.ResultText(string(current.ReviewDecision))
-	// case "state":
-	// 	ctx.ResultText(string(current.State))
-	// case "title":
-	// 	ctx.ResultText(current.Title)
-	// case "updated_at":
-	// 	t := current.UpdatedAt
-	// 	if t.IsZero() {
-	// 		ctx.ResultNull()
-	// 	} else {
-	// 		ctx.ResultText(t.Format(time.RFC3339Nano))
-	// 	}
-	// case "url":
-	// 	ctx.ResultText(current.Url.String())
-	// }
 	return nil
 }
 
-//TODO if results = nil pull the first one but then if the currentpr still has comments left
-// pull comments then pull pr's afterwards
-/*
-*Go through each pr that does not have comments and iterate through in a for loop until you find one with comments. Then
-*
- */
 func (i *iterPRComments) Next() (vtab.Row, error) {
 	// if no results have been pulled... pull them
 	if i.results == nil {
@@ -310,82 +214,7 @@ func (i *iterPRComments) Next() (vtab.Row, error) {
 		}
 	}
 	return nil, io.EOF
-	// if the pageInfo does not have a next page then we are done
 
-	if i.currentPR < len(i.results.Edges) {
-		if len(i.results.Edges[i.currentPR].Comments.Nodes) <= i.currentComment && i.results.Edges[i.currentPR].Comments.PageInfo.HasNextPage {
-			currentComments := i.results.Edges[i.currentPR].Comments
-			err := i.RateLimiter.Wait(context.Background())
-			if err != nil {
-				return nil, err
-			}
-			// if i.results.Edges[i.currentPR].Comments != nil{
-
-			// }
-			var commentCursor *githubv4.String
-			if currentComments.PageInfo.EndCursor != "" {
-				commentCursor = &currentComments.PageInfo.EndCursor
-			}
-			prCursor := i.results.StartCursor
-
-			l := i.logger().With().Interface("commentCursor", commentCursor).Interface("prCursor", prCursor).Logger()
-			l.Info().Msgf("fetching page of comments for %s/%s", i.owner, i.name)
-
-			results, err := i.fetchPRComments(context.Background(), prCursor, commentCursor)
-
-			if err != nil {
-				return nil, err
-			}
-			out, _ := json.Marshal(results)
-			fmt.Println(string(out))
-			i.results = results
-			i.currentComment = 0
-
-			if len(results.Edges[i.currentPR].Comments.Nodes) == 0 {
-				l.Info().Msgf("no comments in pull request 285")
-				i.Next()
-			}
-			return i, nil
-		} else if i.currentPR+1 < len(i.results.Edges) {
-			i.currentPR++
-			i.currentComment = 0
-		}
-	}
-	if i.currentPR+1 < len(i.results.Edges) {
-		i.currentPR += 1
-		return i, nil
-	} else {
-		if i.results.HasNextPage {
-			err := i.RateLimiter.Wait(context.Background())
-			if err != nil {
-				return nil, err
-			}
-
-			var cursor, commentCursor *githubv4.String
-			if i.results != nil {
-				cursor = i.results.EndCursor
-			}
-
-			l := i.logger().With().Interface("commentCursor", commentCursor).Interface("prCursor", cursor).Logger()
-			l.Info().Msgf("fetching page of repo_pull_requests for %s/%s", i.owner, i.name)
-			results, err := i.fetchPRComments(context.Background(), cursor, commentCursor)
-			if err != nil {
-				return nil, err
-			}
-
-			i.results = results
-			i.currentPR = 0
-			i.currentComment = 0
-
-			if len(results.Edges) == 0 {
-				return nil, io.EOF
-			}
-		} else {
-			return nil, io.EOF
-		}
-	}
-
-	return i, nil
 }
 
 var prCommentCols = []vtab.Column{
@@ -400,35 +229,6 @@ var prCommentCols = []vtab.Column{
 	{Name: "c_url", Type: "TEXT"},
 	{Name: "pr_id", Type: "TEXT"},
 	{Name: "pr_number", Type: "INT"},
-	// {Name: "closed", Type: "BOOLEAN"},
-	// {Name: "closed_at", Type: "DATETIME"},
-	// {Name: "comment_count", Type: "INT", OrderBy: vtab.ASC | vtab.DESC},
-	// {Name: "commit_count", Type: "INT"},
-	// {Name: "created_at", Type: "DATETIME", OrderBy: vtab.ASC | vtab.DESC},
-	// {Name: "created_via_email", Type: "BOOLEAN"},
-	// {Name: "database_id", Type: "INT"},
-	// {Name: "deletions", Type: "INT"},
-	// {Name: "editor_login", Type: "TEXT"},
-	// {Name: "head_ref_name", Type: "TEXT"},
-	// {Name: "head_ref_oid", Type: "TEXT"},
-	// {Name: "head_repository_name", Type: "TEXT"},
-	// {Name: "is_draft", Type: "BOOLEAN"},
-	// {Name: "label_count", Type: "INT"},
-	// {Name: "last_edited_at", Type: "DATETIME"},
-	// {Name: "locked", Type: "BOOLEAN"},
-	// {Name: "maintainer_can_modify", Type: "BOOLEAN"},
-	// {Name: "mergeable", Type: "TEXT"},
-	// {Name: "merged", Type: "BOOLEAN"},
-	// {Name: "merged_at", Type: "DATETIME"},
-	// {Name: "merged_by", Type: "TEXT"},
-	// {Name: "number", Type: "INT"},
-	// {Name: "participant_count", Type: "INT"},
-	// {Name: "published_at", Type: "DATETIME"},
-	// {Name: "review_decision", Type: "TEXT"},
-	// {Name: "state", Type: "TEXT"},
-	// {Name: "title", Type: "TEXT"},
-	// {Name: "updated_at", Type: "DATETIME", OrderBy: vtab.ASC | vtab.DESC},
-	// {Name: "url", Type: "TEXT"},
 }
 
 func NewPRCommentsModule(opts *Options) sqlite.Module {
