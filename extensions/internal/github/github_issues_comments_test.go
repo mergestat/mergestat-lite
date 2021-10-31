c759b7acd5a2fdb140cee49e2240d0c8cf756c65
vialeon <dnewberry21@amherst.edu>
2021-10-31 13:17:34 -0500

    update testing

--- extensions/internal/github/github_issues_comments_test.go
+++ extensions/internal/github/github_issues_comments_test.go
@@ -12,22 +12,34 @@ func TestIssueComments(t *testing.T) {
 
 	db := Connect(t, Memory)
 
-	rows, err := db.Query("SELECT * FROM github_issue_comments('askgitdev/askgit') LIMIT 10")
+	rows, err := db.Query("SELECT * FROM github_issue_comments('askgitdev/askgit',10) LIMIT 10")
 	if err != nil {
 		t.Fatalf("failed to execute query: %v", err.Error())
 	}
 	defer rows.Close()
 
-	colCount, content, err := tools.RowContent(rows)
+	colCount, _, err := tools.RowContent(rows)
 	if err != nil {
 		t.Fatalf("failed to retrieve row contents: %v", err.Error())
 	}
 
-	if expected := 10; colCount != expected {
+	if expected := 9; colCount != expected {
 		t.Fatalf("expected %d columns, got: %d", expected, colCount)
 	}
 
-	if expected := 10; len(content) != expected {
-		t.Fatalf("expected %d rows, got: %d", expected, len(content))
+	rows, err = db.Query("SELECT * FROM github_issue_comments('askgitdev','askgit',10) LIMIT 10")
+	if err != nil {
+		t.Fatalf("failed to execute query: %v", err.Error())
 	}
+	defer rows.Close()
+
+	colCount, _, err = tools.RowContent(rows)
+	if err != nil {
+		t.Fatalf("failed to retrieve row contents: %v", err.Error())
+	}
+
+	if expected := 9; colCount != expected {
+		t.Fatalf("expected %d columns, got: %d", expected, colCount)
+	}
+
 }
