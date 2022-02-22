4636ffcf4d8d9478bae4679c1d64fb14684aacf4
dnewberr <derricknewberry2017@gmail.com>
2022-02-21 20:42:42 -0600

    make tests cleaner(table driven)

--- extensions/internal/helpers/print_duration_test.go
+++ extensions/internal/helpers/print_duration_test.go
@@ -1,176 +1,58 @@
 package helpers
 
 import (
+	"database/sql"
 	"testing"
 
 	"github.com/mergestat/mergestat/extensions/internal/tools"
 )
 
 func TestPrintDuration(t *testing.T) {
-	// test multiple years
-	expected := "3 years (33 months)"
-	rows, err := FixtureDatabase.Query(`SELECT printduration('1000')`)
-	if err != nil {
-		t.Fatal(err)
+	type test struct {
+		query    string
+		expected string
 	}
-
-	rowNum, contents, err := tools.RowContent(rows)
-	if err != nil {
-		t.Fatalf("err %d at row Number %d", err, rowNum)
+	tests := []test{
+		// multiple years
+		{query: `SELECT printduration('1000')`, expected: "3 years (33 months)"},
+		// single year
+		{query: `SELECT printduration('365')`, expected: "1 year (12 months)"},
+		// multiple months
+		{query: `SELECT printduration('185')`, expected: "7 months"},
+		// single month
+		{query: `SELECT printduration('31')`, expected: "1 month"},
+		// multiple days
+		{query: `SELECT printduration('5')`, expected: "5 days"},
+		// single day
+		{query: `SELECT printduration('1')`, expected: "1 day"},
+		// multiple hours
+		{query: `SELECT printduration('.5')`, expected: "12 hours"},
+		// single hour
+		{query: `SELECT printduration('.04')`, expected: "1 hour"},
+		// multiple minutes
+		{query: `SELECT printduration('.022')`, expected: "32 minutes"},
+		// single minute
+		{query: `SELECT printduration('.001')`, expected: "1 minute"},
+		// seconds
+		{query: `SELECT printduration('.0001')`, expected: "a few seconds"},
+		// null
+		{query: `SELECT printduration('0')`, expected: "<none>"},
 	}
-	if contents[0][0] != expected {
-		t.Fatalf("expected string: %s, got %s", expected, contents[0][0])
+	var rows *sql.Rows
+	var err error
+	var rowNum int
+	var contents [][]string
+	for _, testCase := range tests {
+		rows, err = FixtureDatabase.Query(testCase.query)
+		if err != nil {
+			t.Fatal(err)
+		}
+		rowNum, contents, err = tools.RowContent(rows)
+		if err != nil {
+			t.Fatalf("err %d at row Number %d", err, rowNum)
+		}
+		if contents[0][0] != testCase.expected {
+			t.Fatalf("expected string: %s, got %s", testCase.expected, contents[0][0])
+		}
 	}
-	// test year
-	expected = "1 year (12 months)"
-	rows, err = FixtureDatabase.Query(`SELECT printduration('365')`)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	rowNum, contents, err = tools.RowContent(rows)
-	if err != nil {
-		t.Fatalf("err %d at row Number %d", err, rowNum)
-	}
-	if contents[0][0] != expected {
-		t.Fatalf("expected string: %s, got %s", expected, contents[0][0])
-	}
-	// test months
-	expected = "7 months"
-	rows, err = FixtureDatabase.Query(`SELECT printduration('185')`)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	rowNum, contents, err = tools.RowContent(rows)
-	if err != nil {
-		t.Fatalf("err %d at row Number %d", err, rowNum)
-	}
-	if contents[0][0] != expected {
-		t.Fatalf("expected string: %s, got %s", expected, contents[0][0])
-	}
-
-	// test month
-	expected = "1 month"
-	rows, err = FixtureDatabase.Query(`SELECT printduration('31')`)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	rowNum, contents, err = tools.RowContent(rows)
-	if err != nil {
-		t.Fatalf("err %d at row Number %d", err, rowNum)
-	}
-	if contents[0][0] != expected {
-		t.Fatalf("expected string: %s, got %s", expected, contents[0][0])
-	}
-	// test days
-	expected = "5 days"
-	rows, err = FixtureDatabase.Query(`SELECT printduration('5')`)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	rowNum, contents, err = tools.RowContent(rows)
-	if err != nil {
-		t.Fatalf("err %d at row Number %d", err, rowNum)
-	}
-	if contents[0][0] != expected {
-		t.Fatalf("expected string: %s, got %s", expected, contents[0][0])
-	}
-	// test day
-	expected = "1 day"
-	rows, err = FixtureDatabase.Query(`SELECT printduration('1')`)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	rowNum, contents, err = tools.RowContent(rows)
-	if err != nil {
-		t.Fatalf("err %d at row Number %d", err, rowNum)
-	}
-	if contents[0][0] != expected {
-		t.Fatalf("expected string: %s, got %s", expected, contents[0][0])
-	}
-	// test hours
-	expected = "12 hours"
-	rows, err = FixtureDatabase.Query(`SELECT printduration('.5')`)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	rowNum, contents, err = tools.RowContent(rows)
-	if err != nil {
-		t.Fatalf("err %d at row Number %d", err, rowNum)
-	}
-	if contents[0][0] != expected {
-		t.Fatalf("expected string: %s, got %s", expected, contents[0][0])
-	}
-	// test hour
-	expected = "1 hour"
-	rows, err = FixtureDatabase.Query(`SELECT printduration('.04')`)
-	if err != nil {
-		t.Fatal(err)
-	}
-	rowNum, contents, err = tools.RowContent(rows)
-	if err != nil {
-		t.Fatalf("err %d at row Number %d", err, rowNum)
-	}
-	if contents[0][0] != expected {
-		t.Fatalf("expected string: %s, got %s", expected, contents[0][0])
-	}
-	// test minutes
-	expected = "32 minutes"
-	rows, err = FixtureDatabase.Query(`SELECT printduration('.022')`)
-	if err != nil {
-		t.Fatal(err)
-	}
-	rowNum, contents, err = tools.RowContent(rows)
-	if err != nil {
-		t.Fatalf("err %d at row Number %d", err, rowNum)
-	}
-	if contents[0][0] != expected {
-		t.Fatalf("expected string: %s, got %s", expected, contents[0][0])
-	}
-	// test minute
-	expected = "1 minute"
-	rows, err = FixtureDatabase.Query(`SELECT printduration('.001')`)
-	if err != nil {
-		t.Fatal(err)
-	}
-	rowNum, contents, err = tools.RowContent(rows)
-	if err != nil {
-		t.Fatalf("err %d at row Number %d", err, rowNum)
-	}
-	if contents[0][0] != expected {
-		t.Fatalf("expected string: %s, got %s", expected, contents[0][0])
-	}
-	// test seconds
-	expected = "a few seconds"
-	rows, err = FixtureDatabase.Query(`SELECT printduration('.0001')`)
-	if err != nil {
-		t.Fatal(err)
-	}
-	rowNum, contents, err = tools.RowContent(rows)
-	if err != nil {
-		t.Fatalf("err %d at row Number %d", err, rowNum)
-	}
-	if contents[0][0] != expected {
-		t.Fatalf("expected string: %s, got %s", expected, contents[0][0])
-	}
-	// test none
-	expected = "<none>"
-	rows, err = FixtureDatabase.Query(`SELECT printduration('0')`)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	rowNum, contents, err = tools.RowContent(rows)
-	if err != nil {
-		t.Fatalf("err %d at row Number %d", err, rowNum)
-	}
-	if contents[0][0] != expected {
-		t.Fatalf("expected string: %s, got %s", expected, contents[0][0])
-	}
-
 }
--- extensions/internal/helpers/timediff_test.go
+++ extensions/internal/helpers/timediff_test.go
@@ -1,6 +1,7 @@
 package helpers
 
 import (
+	"database/sql"
 	"testing"
 	"time"
 
@@ -8,245 +9,55 @@ import (
 )
 
 func TestTimediff(t *testing.T) {
-	// test multiple years
-	expected := "5 years ago"
-	rows, err := FixtureDatabase.Query(`SELECT timediff(DATE('now','-4 year','-6 month'),DATE('now'),'2006-01-02')`)
-	if err != nil {
-		t.Fatal(err)
+	type test struct {
+		query      string
+		queryInput []interface{}
+		expected   string
 	}
+	tests := []test{
+		// 3 inputs end date, start date, format
+		{query: `SELECT timediff(DATE('now','-4 year','-6 month'),DATE('now'),'2006-01-02')`, expected: "5 years ago"},
+		{query: `SELECT timediff(DATE('now','-1 year'),DATE('now'),'2006-01-02')`, expected: "a year ago"},
+		{query: `SELECT timediff(DATE('now','-6 month'),DATE('now'),'2006-01-02')`, expected: "7 months ago"},
+		{query: `SELECT timediff(DATE('now','-1 month'),DATE('now'),'2006-01-02')`, expected: "a month ago"},
+		{query: `SELECT timediff(DATE('now','-5 day'),DATE('now'),'2006-01-02')`, expected: "5 days ago"},
+		{query: `SELECT timediff(DATE('now','-1 day'),DATE('now'),'2006-01-02')`, expected: "a day ago"},
+		// one input end date
+		{query: `SELECT timediff(?)`, queryInput: []interface{}{time.Now().Add(-time.Hour * 7).Add(time.Minute * 1).Format(time.RFC3339)}, expected: "7 hours ago"},
+		{query: `SELECT timediff(?)`, queryInput: []interface{}{time.Now().Add(-time.Hour * 1).Format(time.RFC3339)}, expected: "an hour ago"},
+		{query: `SELECT timediff(?)`, queryInput: []interface{}{time.Now().Add(-time.Minute * 32).Add(time.Second * 2).Format(time.RFC3339)}, expected: "32 minutes ago"},
+		{query: `SELECT timediff(?)`, queryInput: []interface{}{time.Now().Add(-time.Minute * 1).Format(time.RFC3339)}, expected: "a minute ago"},
+		{query: `SELECT timediff(?)`, queryInput: []interface{}{time.Now().Add(-time.Second * 15).Format(time.RFC3339)}, expected: "a few seconds ago"},
+		// two inputs end date, start date
+		{query: `SELECT timediff(?,?)`, queryInput: []interface{}{time.Now().Add(-time.Second * 15).Format(time.RFC3339), time.Now().Format(time.RFC3339)}, expected: "a few seconds ago"},
+		{query: `SELECT timediff(?,?)`, queryInput: []interface{}{time.Now().Add(-time.Minute * 1).Format(time.RFC3339), time.Now().Format(time.RFC3339)}, expected: "a minute ago"},
+		{query: `SELECT timediff(?,?)`, queryInput: []interface{}{time.Now().Add(-time.Minute * 32).Format(time.RFC3339), time.Now().Format(time.RFC3339)}, expected: "32 minutes ago"},
+		{query: `SELECT timediff(?,?)`, queryInput: []interface{}{time.Now().Add(-time.Hour * 1).Format(time.RFC3339), time.Now().Format(time.RFC3339)}, expected: "an hour ago"},
+	}
+	var rows *sql.Rows
+	var err error
+	var rowNum int
+	var contents [][]string
+	for _, testCase := range tests {
+		if testCase.queryInput != nil {
+			rows, err = FixtureDatabase.Query(testCase.query, testCase.queryInput...)
+			if err != nil {
+				t.Fatal(err)
+			}
+		} else {
+			rows, err = FixtureDatabase.Query(testCase.query)
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
 
-	rowNum, contents, err := tools.RowContent(rows)
-	if err != nil {
-		t.Fatalf("err %d at row Number %d", err, rowNum)
-	}
-	if contents[0][0] != expected {
-		t.Fatalf("expected string: %s, got %s", expected, contents[0][0])
-	}
-	// test year
-	expected = "a year ago"
-	rows, err = FixtureDatabase.Query(`SELECT timediff(DATE('now','-1 year'),DATE('now'),'2006-01-02')`)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	rowNum, contents, err = tools.RowContent(rows)
-	if err != nil {
-		t.Fatalf("err %d at row Number %d", err, rowNum)
-	}
-	if contents[0][0] != expected {
-		t.Fatalf("expected string: %s, got %s", expected, contents[0][0])
-	}
-	// test months
-	expected = "7 months ago"
-	rows, err = FixtureDatabase.Query(`SELECT timediff(DATE('now','-6 month'),DATE('now'),'2006-01-02')`)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	rowNum, contents, err = tools.RowContent(rows)
-	if err != nil {
-		t.Fatalf("err %d at row Number %d", err, rowNum)
-	}
-	if contents[0][0] != expected {
-		t.Fatalf("expected string: %s, got %s", expected, contents[0][0])
-	}
-
-	// test month
-	expected = "a month ago"
-	rows, err = FixtureDatabase.Query(`SELECT timediff(DATE('now','-1 month'),DATE('now'),'2006-01-02')`)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	rowNum, contents, err = tools.RowContent(rows)
-	if err != nil {
-		t.Fatalf("err %d at row Number %d", err, rowNum)
-	}
-	if contents[0][0] != expected {
-		t.Fatalf("expected string: %s, got %s", expected, contents[0][0])
-	}
-	// test days
-	expected = "5 days ago"
-	rows, err = FixtureDatabase.Query(`SELECT timediff(DATE('now','-5 day'),DATE('now'),'2006-01-02')`)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	rowNum, contents, err = tools.RowContent(rows)
-	if err != nil {
-		t.Fatalf("err %d at row Number %d", err, rowNum)
-	}
-	if contents[0][0] != expected {
-		t.Fatalf("expected string: %s, got %s", expected, contents[0][0])
-	}
-	// test day
-	expected = "a day ago"
-	rows, err = FixtureDatabase.Query(`SELECT timediff(DATE('now','-1 day'),DATE('now'),'2006-01-02')`)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	rowNum, contents, err = tools.RowContent(rows)
-	if err != nil {
-		t.Fatalf("err %d at row Number %d", err, rowNum)
-	}
-	if contents[0][0] != expected {
-		t.Fatalf("expected string: %s, got %s", expected, contents[0][0])
-	}
-	// test hours
-	testTime := time.Now().Add(-time.Hour * 7).Add(time.Minute * 1)
-
-	expected = "7 hours ago"
-	rows, err = FixtureDatabase.Query(`SELECT timediff(?)`, testTime.Format(time.RFC3339))
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	rowNum, contents, err = tools.RowContent(rows)
-	if err != nil {
-		t.Fatalf("err %d at row Number %d", err, rowNum)
-	}
-	if contents[0][0] != expected {
-		t.Fatalf("expected string: %s, got %s", expected, contents[0][0])
-	}
-	// test hour
-	testTime = time.Now().Add(-time.Hour * 1)
-
-	expected = "an hour ago"
-	rows, err = FixtureDatabase.Query(`SELECT timediff(?)`, testTime.Format(time.RFC3339))
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	rowNum, contents, err = tools.RowContent(rows)
-	if err != nil {
-		t.Fatalf("err %d at row Number %d", err, rowNum)
-	}
-	if contents[0][0] != expected {
-		t.Fatalf("expected string: %s, got %s", expected, contents[0][0])
-	}
-	// test minutes
-	testTime = time.Now().Add(-time.Minute * 32).Add(time.Second * 2)
-	expected = "32 minutes ago"
-	rows, err = FixtureDatabase.Query(`SELECT timediff(?)`, testTime.Format(time.RFC3339))
-	if err != nil {
-		t.Fatal(err)
-	}
-	rowNum, contents, err = tools.RowContent(rows)
-	if err != nil {
-		t.Fatalf("err %d at row Number %d", err, rowNum)
-	}
-	if contents[0][0] != expected {
-		t.Fatalf("expected string: %s, got %s", expected, contents[0][0])
-	}
-	// test minute
-	testTime = time.Now().Add(-time.Minute * 1)
-
-	expected = "a minute ago"
-	rows, err = FixtureDatabase.Query(`SELECT timediff(?)`, testTime.Format(time.RFC3339))
-	if err != nil {
-		t.Fatal(err)
-	}
-	rowNum, contents, err = tools.RowContent(rows)
-	if err != nil {
-		t.Fatalf("err %d at row Number %d", err, rowNum)
-	}
-	if contents[0][0] != expected {
-		t.Fatalf("expected string: %s, got %s", expected, contents[0][0])
-	}
-	// test seconds
-	testTime = time.Now().Add(-time.Second * 15)
-
-	expected = "a few seconds ago"
-	rows, err = FixtureDatabase.Query(`SELECT timediff(?)`, testTime.Format(time.RFC3339))
-	if err != nil {
-		t.Fatal(err)
-	}
-	rowNum, contents, err = tools.RowContent(rows)
-	if err != nil {
-		t.Fatalf("err %d at row Number %d", err, rowNum)
-	}
-	if contents[0][0] != expected {
-		t.Fatalf("expected string: %s, got %s", expected, contents[0][0])
-	}
-	// testing for 2 inputs as well
-	// test hours
-	testTime = time.Now().Add(-time.Hour * 7)
-	currentTime := time.Now()
-	expected = "7 hours ago"
-	rows, err = FixtureDatabase.Query(`SELECT timediff(?,?)`, testTime.Format(time.RFC3339), currentTime.Format(time.RFC3339))
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	rowNum, contents, err = tools.RowContent(rows)
-	if err != nil {
-		t.Fatalf("err %d at row Number %d", err, rowNum)
-	}
-	if contents[0][0] != expected {
-		t.Fatalf("expected string: %s, got %s", expected, contents[0][0])
-	}
-	// test hour
-	testTime = time.Now().Add(-time.Hour * 1)
-	currentTime = time.Now()
-	expected = "an hour ago"
-	rows, err = FixtureDatabase.Query(`SELECT timediff(?,?)`, testTime.Format(time.RFC3339), currentTime.Format(time.RFC3339))
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	rowNum, contents, err = tools.RowContent(rows)
-	if err != nil {
-		t.Fatalf("err %d at row Number %d", err, rowNum)
-	}
-	if contents[0][0] != expected {
-		t.Fatalf("expected string: %s, got %s", expected, contents[0][0])
-	}
-	// test minutes
-	testTime = time.Now().Add(-time.Minute * 32)
-	currentTime = time.Now()
-	expected = "32 minutes ago"
-	rows, err = FixtureDatabase.Query(`SELECT timediff(?,?)`, testTime.Format(time.RFC3339), currentTime.Format(time.RFC3339))
-	if err != nil {
-		t.Fatal(err)
-	}
-	rowNum, contents, err = tools.RowContent(rows)
-	if err != nil {
-		t.Fatalf("err %d at row Number %d", err, rowNum)
-	}
-	if contents[0][0] != expected {
-		t.Fatalf("expected string: %s, got %s", expected, contents[0][0])
-	}
-	// test minute
-	testTime = time.Now().Add(-time.Minute * 1)
-	currentTime = time.Now()
-	expected = "a minute ago"
-	rows, err = FixtureDatabase.Query(`SELECT timediff(?,?)`, testTime.Format(time.RFC3339), currentTime.Format(time.RFC3339))
-	if err != nil {
-		t.Fatal(err)
-	}
-	rowNum, contents, err = tools.RowContent(rows)
-	if err != nil {
-		t.Fatalf("err %d at row Number %d", err, rowNum)
-	}
-	if contents[0][0] != expected {
-		t.Fatalf("expected string: %s, got %s", expected, contents[0][0])
-	}
-	// test seconds
-	testTime = time.Now().Add(-time.Second * 15)
-	currentTime = time.Now()
-
-	expected = "a few seconds ago"
-	rows, err = FixtureDatabase.Query(`SELECT timediff(?,?)`, testTime.Format(time.RFC3339), currentTime.Format(time.RFC3339))
-	if err != nil {
-		t.Fatal(err)
-	}
-	rowNum, contents, err = tools.RowContent(rows)
-	if err != nil {
-		t.Fatalf("err %d at row Number %d", err, rowNum)
-	}
-	if contents[0][0] != expected {
-		t.Fatalf("expected string: %s, got %s", expected, contents[0][0])
+		rowNum, contents, err = tools.RowContent(rows)
+		if err != nil {
+			t.Fatalf("err %d at row Number %d", err, rowNum)
+		}
+		if contents[0][0] != testCase.expected {
+			t.Fatalf("expected string: %s, got %s", testCase.expected, contents[0][0])
+		}
 	}
 
 }
