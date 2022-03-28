fba937637eb0a527bf5c1285cde532e7850ef19f
Patrick DeVivo <patrick.devivo@gmail.com>
2022-03-28 10:15:46 -0400

    feat: add table-valued-function for listing GitHub PR reviews

---
+++ extensions/internal/github/pr_reviews_test.go
@@ -0,0 +1,44 @@
+package github_test
+
+import (
+	"testing"
+
+	"github.com/mergestat/mergestat/extensions/internal/tools"
+)
+
+func TestPRReviews(t *testing.T) {
+	cleanup := newRecorder(t)
+	defer cleanup()
+
+	db := Connect(t, Memory)
+
+	rows, err := db.Query("SELECT * FROM github_repo_pr_reviews('mergestat/mergestat', 261) LIMIT 5")
+	if err != nil {
+		t.Fatalf("failed to execute query: %v", err.Error())
+	}
+	defer rows.Close()
+
+	colCount, _, err := tools.RowContent(rows)
+	if err != nil {
+		t.Fatalf("failed to retrieve row contents: %v", err.Error())
+	}
+
+	if expected := 14; colCount != expected {
+		t.Fatalf("expected %d columns, got: %d", expected, colCount)
+	}
+
+	rows, err = db.Query("SELECT * FROM github_repo_pr_reviews('mergestat', 'mergestat', 261) LIMIT 5")
+	if err != nil {
+		t.Fatalf("failed to execute query: %v", err.Error())
+	}
+	defer rows.Close()
+
+	colCount, _, err = tools.RowContent(rows)
+	if err != nil {
+		t.Fatalf("failed to retrieve row contents: %v", err.Error())
+	}
+
+	if expected := 14; colCount != expected {
+		t.Fatalf("expected %d columns, got: %d", expected, colCount)
+	}
+}
